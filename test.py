9557acde2f1afc8c9ea0b5b1b00e3d918af4574b
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-01-25 18:32:32 -0500

    fixed

--- test.py
+++ test.py
@@ -1,23 +1,27 @@
-"""
-pytest
-"""
+#from django.http import response
 from app import app
 
+
+#Hi there I am running a test
 def test1():
     """
-    This function tests that the flask applicationhas a coorect response code when the application goes live.
+    This function tests that that flask application has a correct response code
+    when the application goes live.    
     """
     response = app.test_client().get("/")
     assert response.status_code == 200
-
+    
 def test2():
     """A dummy docstring."""
     response = app.test_client().get("/edit")
-    assert response.status_code ==200
-
+    assert response.status_code == 200
+    
 def test3():
     """A dummy docstring."""
     response = app.test_client().get("/edit")
     assert b"To Do App" in response.data
     assert b"Todo Title" in response.data
     assert b"Add" in response.data
+    # assert b"Project" in response.data
+
+
